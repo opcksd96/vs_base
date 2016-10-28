8f3c3ae3a896d034d61bbc80776a5cdcbbed2f4b
owner <ConstanceWeelkes@hotmail.com>
2016-10-29 01:08:15 +0900

    jQuery導入

--- BlankCordovaApp1/www/scripts/index.js
+++ BlankCordovaApp1/www/scripts/index.js
@@ -1,7 +1,9 @@
 // 空白のテンプレートの概要については、次のドキュメントを参照してください:
 // http://go.microsoft.com/fwlink/?LinkID=397704
-// ページ上のコードをデバッグするには、Ripple で読み込むか、Android デバイス/エミュレーターで読み込みます。アプリを起動し、ブレークポイントを設定します。
+// ページ上のコードをデバッグするには、Ripple で読み込むか、Android デバイス/エミュレーターで読み込みます。
+// アプリを起動し、ブレークポイントを設定します。
 // 次に、JavaScript コンソールで "window.location.reload()" を実行します。
+
 (function () {
     "use strict";
 
@@ -10,8 +12,8 @@
     function onDeviceReady() {
         // Cordova の一時停止を処理し、イベントを再開します
         document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener('resume', onResume.bind(this), false);
+
         // TODO: Cordova が読み込まれました。ここで、Cordova を必要とする初期化を実行します。
         var parentElement = document.getElementById('deviceready');
         var listeningElement = parentElement.querySelector('.listening');
@@ -37,6 +39,7 @@
             // HH:mm:ssという書式で現在時刻を表示する
             parentElement.innerHTML = hh + ":" + mm + ":" + ss;
         }
+
     };
 
     function onPause() {
